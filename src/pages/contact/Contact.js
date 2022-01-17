92a2ff3ba51bbdcd0957174d86e5887329a1258f
Charles Kanoy <ch.kanoy@gmail.com>
2022-01-16 18:19:09 -0600

    removed commented section

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -4,15 +4,6 @@ import ReactMarkdown from 'react-markdown';
 import './Contact.css';
 
 function Contact() {
-    // return (
-    //     <div>
-    //         <h1>Looking to get ahold of me?</h1>
-    //         <hr/>
-    //         <div className="about">
-    //             Email me at <a className="email" href={MAIL_TO}>{EMAIL}</a>
-    //         </div>
-    //     </div>
-    // );
     const [homeContent, setHomeContent] = useState("");
     
     useEffect(() => {
