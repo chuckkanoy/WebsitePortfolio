243f2e027f14df250564b7ddecb4243fd111cd2d
Charles Kanoy <ch.kanoy@gmail.com>
2023-01-24 23:42:10 -0600

    Fixing react-dom related Netlify issue

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client'
+import ReactDOM from 'react-dom';
+// import {createRoot} from 'react-dom/client'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement)
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement)
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
